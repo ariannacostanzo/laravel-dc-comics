37ff5729a939eb86c10860a8f4e0bd636365d157
ariannacostanzo <costanzo.arianna@outlook.it>
2024-03-13 14:58:23 +0100

    creating table of comics

--- database/migrations/2024_03_13_134000_create_comics_table.php
+++ database/migrations/2024_03_13_134000_create_comics_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Comic;
 
 return new class extends Migration
 {
@@ -13,6 +14,19 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+
+
+            $table->string('title', 150);
+            $table->text('description');
+            $table->text('thumb');
+            $table->string('price', 12);
+            $table->string('series', 50);
+            $table->date('sale_date');
+            $table->string('type', 50);
+            // $table->array('artists');
+            // $table->array('writers');
+
+
             $table->timestamps();
         });
     }
